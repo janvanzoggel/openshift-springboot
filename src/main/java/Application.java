723e28c272d4d2f1d38634964764629d76f41550
jan@rubix.nl <jan@rubix.nl>
2017-07-18 22:57:57 +0200

    New rest interface

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,9 +2,15 @@ import hello.wsdl.GetQuoteResponse;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
