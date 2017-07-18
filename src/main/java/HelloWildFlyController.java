278a7147fc667638556af23ceb3c8836037cceda
jan@rubix.nl <jan@rubix.nl>
2017-07-18 22:52:49 +0200

    New rest interface

---
+++ src/main/java/HelloWildFlyController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWildFlyController
+{
+    @RequestMapping("hello")
+    public String sayHello()
+    {
+        return ("Hi, this is SpringBoot on WildFly with OpenShift S2I");
+    }
+
+}
